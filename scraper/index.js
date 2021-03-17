84d0a7583db6980cd1c7611668b1912aa9395441
unitgit7 <>
2021-03-17 00:59:17 -0700

    getlementsbyclassname working

--- scraper/index.js
+++ scraper/index.js
@@ -1,28 +1,35 @@
 const { app, BrowserWindow } = require('electron')
 const fs = require('fs')
-
 const JAVASCRIPT = `
 function main() {
-  alert("Hello Bill");
+  let message = "";
   try {
-    return document.getElementsByClassName("SarWorkspace_narrativeTextArea__N6mua")[0].value;
+    const divs = document.getElementsByClassName("CollapsibleFormSection_headerTopLeft__18kl0");
+    for (var i = 0; i < divs.length; i += 1) {
+      const div = divs[i];
+      if (div.innerHTML.startsWith("Step 5")) {
+        return div.innerHTML;
+      }
+    }
   } catch (e) {
-    return "title is undefined";
+    message = e.toString();
   }
+  return message;
 }
 main();
 `.trim();
 
-
 async function javascriptLoop(win) {
   while(true) {
     await new Promise((resolve, reject) => setTimeout(resolve, 5000));
-    const response = await win.webContents.executeJavaScript(JAVASCRIPT);
-    // fs.writeFile('/Users/wdestein/tmp/jana', response);
-    fs.writeFileSync('', `response: ${response}`);
+    try {
+      const response = await win.webContents.executeJavaScript(JAVASCRIPT);
+      console.log(JSON.stringify(response));
+    } catch(e) {
+      console.log(e.toString());
+    }
   }
 }
-
 function createWindow () {
   const win = new BrowserWindow({
     width: 800,
@@ -32,47 +39,20 @@ function createWindow () {
       devTools: true
     }
   })
-
   win.webContents.openDevTools({mode: 'detach'});
-
   // Load the website
-  win.loadURL('URL')
-
+  win.loadURL('https://dashboard.unit21.com/filings/sar/1895074')
   // Run the javascript loop
   javascriptLoop(win);
 }
-
 app.whenReady().then(createWindow)
-
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
-
 app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow()
   }
 })
-
-// The below part returned some document names successfully:
-
-// const JAVASCRIPT = `
-// function main() {
-//   alert("Hello Bill");
-//   try {
-//     var elems = document.getElementsByClassName("AttachmentItem_attachmentName__3BpVZ");
-//     var result = [];
-//     for(var i=0; i<elems.length; i++) {
-//       result.push(elems[i].innerHTML)
-//     }
-//     return result;
-//
-//
-//   } catch (e) {
-//     return "title is undefined";
-//   }
-// }
-// main();
-// `.trim();
